158665e77512f7693b4e8883ba274e5bdd797462
Abrar Riyad <rmd394195@gmail.com>
2026-09-23 10:53:26 +0600

    'up'

---
+++ C Programming/Day-1/fomat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("----------------------------------------\n");
+    printf("%-15s %-15s %-10s\n","Name","Depertment","Salary");
+    printf("----------------------------------------\n");
+    printf("%-15s %-15s %-10s\n","Riyad","SWE","$500");
+    printf("%-15s %-15s %-10s\n","Shown","Cyber securty","$400");
+    printf("%-15s %-15s %-10s\n","Riyajul","DEVOPS","$300");
+    return 0;
+}
