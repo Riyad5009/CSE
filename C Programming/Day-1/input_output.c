986a9f4485bdba08bf523eb3db4e66c240270c71
Abrar Riyad <rmd394195@gmail.com>
2026-09-23 10:31:13 +0600

    'up'

--- C Programming/Day-1/input_output.c
+++ C Programming/Day-1/input_output.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main(){
     int age;
-    char name [20];
+    char name[20];
     double bmi;
-
     printf("Enter your name:");
     scanf("%s",name);
     printf("Enter your age:");
     scanf("%d",&age);
-    printf("Ener your bmi:");
-    scanf("%lf",&bmi);
-    printf("Name: %s\nAge:%d\nBmi:%lf",name,age,bmi);
+    printf("Enter your bmi:");
+    printf("\nName:%s\nAge:%d",name,age);
     return 0;
 }
